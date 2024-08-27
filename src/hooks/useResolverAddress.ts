2ed0b2b14aaf6748dcdc9c1ab7af20320fa9ded8
peripheralist <peripheralist@gmail.com>
2024-08-27 16:59:21 -0600

    resolver bump

--- src/hooks/useResolverAddress.ts
+++ src/hooks/useResolverAddress.ts
@@ -3,7 +3,7 @@ import { ChainId, useChain } from "./useChain";
 
 const addresses: Record<ChainId, `0x${string}`> = {
   [mainnet.id]: "0xasdf", // TODO
-  [sepolia.id]: "0xcD2b6C7678B134098467AB9F37Cf54b7e03D8C41",
+  [sepolia.id]: "0x9499f78B1d237615Ce5DbE0E7d7B79200F740e8C",
 };
 
 export function useResolverAddress() {
