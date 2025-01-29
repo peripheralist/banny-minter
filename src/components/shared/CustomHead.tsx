3f23586863e6f485642e2e1cc3842990a8836721
peripheralist <peripheralist@gmail.com>
2025-01-29 16:08:55 -0700

    -

--- src/components/shared/CustomHead.tsx
+++ src/components/shared/CustomHead.tsx
@@ -8,16 +8,18 @@ export default function CustomHead({
   title?: string;
   description?: string;
 }) {
-  const _title = title ?? "BannyRetail";
+  const _title = title ?? "Banny Retail";
 
   return (
     <Head>
       <title>{_title}</title>
       <meta property="og:title" content={_title} />
+      <meta property="twitter:title" content={_title} />
       {description && (
         <>
-          <meta property="og:description" content={description} />
           <meta name="description" content={description} />
+          <meta property="og:description" content={description} />
+          <meta property="twitter:description" content={description} />
         </>
       )}
     </Head>
