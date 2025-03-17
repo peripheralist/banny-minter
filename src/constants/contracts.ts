c66b75b368353d6b8bdc46dc29ed52c091c7e736
peripheralist <peripheralist@gmail.com>
2025-03-17 15:59:33 -0600

    update resolver

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -17,7 +17,7 @@ export const BAN_REVNET_IDS = (chainId: number) => {
 export const BAN_HOOK = "0x2da41cdc79ae49f2725ab549717b2dbcfc42b958" as Address;
 
 export const RESOLVER_ADDRESS =
-  "0xff80c37a57016eff3d19fb286e9c740ec4537dd3" as Address;
+  "0xa5f8911d4cfd60a6697479f078409434424fe666" as Address;
 
 export const TERMINAL_ADDRESS = process.env
   .NEXT_PUBLIC_TERMINAL_ADDRESS! as Address;
