c88519ae12c6bec007a239403299be067d30d46b
peripheralist <peripheralist@gmail.com>
2025-04-23 12:52:05 -0600

    show/hide bannys in shop

--- src/pages/drops/[dropId].tsx
+++ src/pages/drops/[dropId].tsx
@@ -2,6 +2,7 @@ import { CategoryGroupGrid } from "@/components/shared/CategoryGroupGrid";
 import CustomHead from "@/components/shared/CustomHead";
 import FullscreenLoading from "@/components/shared/FullscreenLoading";
 import RoundedFrame from "@/components/shared/RoundedFrame";
+import TierImage from "@/components/shared/TierImage";
 import TierShopButton from "@/components/shared/TierShopButton";
 import ToolbarBagView from "@/components/shared/ToolbarBagView";
 import { COLORS } from "@/constants/colors";
@@ -11,9 +12,11 @@ import { useAllTiers } from "@/hooks/queries/useAllTiers";
 import { useWindowSize } from "@/hooks/useWindowSize";
 import { ROUTES } from "@/utils/routes";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 export default function Drop() {
+  const [hideBannys, setHideBannys] = useState(false);
+
   const router = useRouter();
 
   const dropId = useMemo(() => {
@@ -68,48 +71,103 @@ export default function Drop() {
                       marginBottom: 48,
                       height: "auto",
                     }}
-                    style={{ padding: 24 }}
+                    style={{ padding: 24, color: COLORS.blue600 }}
                   >
-                    <div style={{ marginBottom: 24 }}>
-                      <h1
+                    {hideBannys ? (
+                      <div
                         style={{
-                          fontSize: FONT_SIZE["2xl"],
-                          color: COLORS.blue600,
+                          display: "flex",
+                          alignItems: "baseline",
+                          gap: 12,
+                          cursor: "pointer",
                         }}
+                        onClick={() => setHideBannys(false)}
                       >
-                        It{"'"}s dangerous to go alone. Take one of these!
-                      </h1>
-                      <p style={{ marginTop: 12, color: COLORS.blue500 }}>
-                        You{"'"}ll need a Banny to wear and preview Drop items.
-                        Add a Banny to your bag to get started.
-                      </p>
-                    </div>
+                        <div
+                          style={{
+                            width: 80,
+                            height: 24,
+                            margin: -20,
+                          }}
+                        >
+                          <TierImage
+                            tier={parsedTiers.find((t) => t.tierId === 4)}
+                            size={80}
+                          />
+                        </div>
+                        <div style={{ flex: 1 }}>Looking for Banny?</div>
+                        <div
+                          style={{
+                            lineHeight: 0,
+                            height: 0,
+                            fontSize: FONT_SIZE["2xl"],
+                            transform: `rotate(180deg) translate(0, 8px)`,
+                          }}
+                        >
+                          ^
+                        </div>
+                      </div>
+                    ) : (
+                      <>
+                        <div style={{ marginBottom: 24 }}>
+                          <div
+                            style={{
+                              textAlign: "right",
+                              cursor: "pointer",
+                              height: 0,
+                              fontSize: FONT_SIZE["2xl"],
+                            }}
+                            onClick={() => setHideBannys(true)}
+                          >
+                            ^
+                          </div>
+                          <h1 style={{ fontSize: FONT_SIZE["2xl"] }}>
+                            It{"'"}s dangerous to go alone. Take one of these!
+                          </h1>
+                          <p
+                            style={{
+                              marginTop: 12,
+                              color: COLORS.blue500,
+                              lineHeight: 1.25,
+                            }}
+                          >
+                            You{"'"}ll need a Banny to wear and preview Drop
+                            items. Add a Banny to your bag to get started.
+                          </p>
+                        </div>
 
-                    <CategoryGroupGrid
-                      items={parsedTiers.filter((t) => t.category === "body")}
-                      excludeGroups={[
-                        "head",
-                        "outfit",
-                        "special",
-                        "background",
-                      ]}
-                      render={(t) => (
-                        <TierShopButton
-                          key={t.tierId}
-                          tier={t}
-                          buttonSize={isSmallScreen ? imgSize - 24 : imgSize}
-                          onClick={() =>
-                            ROUTES.toTier({ router, tierId: t.tierId })
-                          }
+                        <CategoryGroupGrid
+                          items={parsedTiers.filter(
+                            (t) => t.category === "body"
+                          )}
+                          excludeGroups={[
+                            "head",
+                            "outfit",
+                            "special",
+                            "background",
+                          ]}
+                          render={(t) => (
+                            <TierShopButton
+                              key={t.tierId}
+                              tier={t}
+                              buttonSize={
+                                isSmallScreen ? imgSize - 24 : imgSize
+                              }
+                              onClick={() =>
+                                ROUTES.toTier({ router, tierId: t.tierId })
+                              }
+                            />
+                          )}
+                          gridStyle={{
+                            gridTemplateColumns: `repeat(auto-fit, ${
+                              isSmallScreen ? imgSize - 24 : imgSize
+                            }px)`,
+                            gap: 4,
+                            paddingTop: 0,
+                          }}
                         />
-                      )}
-                      gridStyle={{
-                        gridTemplateColumns: `repeat(auto-fit, ${
-                          isSmallScreen ? imgSize - 24 : imgSize
-                        }px)`,
-                        gap: 4,
-                      }}
-                    />
+                      </>
+                    )}
                   </RoundedFrame>
 
                   <div
