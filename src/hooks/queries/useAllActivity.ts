ef63396342a3e33650809112423ad3c2ed4c614d
peripheralist <peripheralist@gmail.com>
2025-12-22 16:30:19 -0700

    omit all pre-v5 activity

--- src/hooks/queries/useAllActivity.ts
+++ src/hooks/queries/useAllActivity.ts
@@ -9,6 +9,7 @@ export function useAllActivity() {
     variables: {
       where: {
         suckerGroupId: BAN_SUCKER_GROUP_ID,
+        timestamp_gt: 1765261735, // omit all pre-v5 activity
         OR: [{ payEvent_not: null }, { decorateBannyEvent_not: null }],
       },
       orderBy: "timestamp",
